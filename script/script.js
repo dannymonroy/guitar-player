75e2b6599a81230d7bb80f152cfe3fc2d8e60a23
Danny Monroy <danny.p.monroy@gmail.com>
2017-07-05 16:26:47 +0100

    Added more notes, changed style, set a timeout for transition

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
 
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const note = document.querySelector(`.note[data-key="${e.keyCode}"]`);
   if(!audio) return;
+  audio.currentTime = 0;
   audio.play();
+  note.classList.add('playing');
+
+  setTimeout(function(){
+    note.classList.remove('playing');
+  }, 300);
+
 });
+/*
+function removeTransition(e) {
+  console.log(e);
+}
+
+const keys = document.querySelectorAll('.note');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+*/
